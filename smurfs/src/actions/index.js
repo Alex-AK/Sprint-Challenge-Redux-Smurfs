66b2eaca8fc8b564857c1ffb858ac23d82fe18fb
Kingaa6 <Kingaa6@gmail.com>
2019-02-01 08:17:48 -0800

    Add redux boilerplate

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,24 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+export const GET_SMURFS = 'GET_SMURFS';
+export const GET_SMURFS_COMPLETE = 'GET_SMURFS_COMPLETE';
+export const GET_SMURFS_FAIL = 'GET_SMURFS_FAIL';
+
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADD_SMURF_COMPLETE = 'ADD_SMURF_COMPLETE';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const UPDATE_SMURF_COMPLETE = 'UPDATE_SMURF_COMPLETE';
+export const UPDATE_SMURF_FAIL = 'UPDATE_SMURF_FAIL';
+
+export const DELETE_SMURF = 'DELETE_SMURF';
+export const DELETE_SMURF_COMPLETE = 'DELETE_SMURF_COMPLETE';
+export const DELETE_SMURF_FAIL = 'DELETE_SMURF_FAIL';
+
+const addSmurf = () => dispatch => {};
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,40 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+import {
+  GET_SMURFS,
+  GET_SMURFS_COMPLETE,
+  GET_SMURFS_FAIL,
+  ADD_SMURF,
+  ADD_SMURF_COMPLETE,
+  ADD_SMURF_FAIL,
+  DELETE_SMURF,
+  DELETE_SMURF_COMPLETE,
+  DELETE_SMURF_FAIL,
+  UPDATE_SMURF,
+  UPDATE_SMURF_COMPLETE,
+  UPDATE_SMURF_FAIL
+} from '../actions';
+
+const initialState = {
+  smurfs: [],
+  isLoading: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: ''
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+};
 
 /*
   You'll only need one smurf reducer for this project.
